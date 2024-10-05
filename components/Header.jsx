865d5eb01c82df57329652a748ef30a8a7fb5988
Puso90 <juwishbrands@gmail.com>
2024-10-05 09:10:51 +0200

    move header elements to components folder

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function header() {
+    return (
+        <header>
+                <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
+                <Link to="/about">About</Link>
+                <Link to="/vans">Vans</Link>
+            </nav>
+        </header>
+    )
+    
+}
