70dec87687b0ae4606831fee78283dc172d616ed
Puso90 <juwishbrands@gmail.com>
2024-10-05 10:50:17 +0200

    Nested Routes

--- index.jsx
+++ index.jsx
@@ -6,6 +6,7 @@ import About from "./pages/About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
 import Layout from "./components/Layout"
+import HostLayout from './components/HostLayout';
 
 import "./server"
 import Dashboard from "./pages/Host/Dashboard";
@@ -19,14 +20,18 @@ function App() {
         <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/vans" element={<Vans />} />
-            <Route path="/vans/:id" element={<VanDetail />} />
 
-          <Route path="/host" element={<Dashboard />}>
-              <Route path="/host/income" element={<Income />} />
-              <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="vans">
+            <Route index element={<Vans />} />
+            <Route path=":id" element={<VanDetail />} />
           </Route>
-          
+
+          <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+          </Route>
+
         </Route>
       </Routes>
     </BrowserRouter>
