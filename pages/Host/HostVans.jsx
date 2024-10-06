9d172809454f3a343f9b6233e87bcbedea0aa7d3
Puso90 <juwishbrands@gmail.com>
2024-10-06 15:07:57 +0200

    last touches

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,18 +1,30 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { getHostVans } from "../../api"
 
 export default function HostVans() {
     const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
 
     React.useEffect(() => {
-        fetch("/api/host/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getHostVans()
+                setVans(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadVans()
     }, [])
 
     const hostVansEls = vans.map(van => (
         <Link
-            to={`/host/vans/${van.id}`}
+            to={van.id}
             key={van.id}
             className="host-van-link-wrapper"
         >
@@ -20,19 +32,25 @@ export default function HostVans() {
                 <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
                 <div className="host-van-info">
                     <h3>{van.name}</h3>
-                    <p>R{van.price}/day</p>
+                    <p>${van.price}/day</p>
                 </div>
             </div>
         </Link>
     ))
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
+    }
 
     return (
         <section>
             <h1 className="host-vans-title">Your listed vans</h1>
             <div className="host-vans-list">
                 {
-                    //Check if host has indeed uploaded vans on website
-                    //The > helps make sure it doesn't fall into default of Loading... Forever!
                     vans.length > 0 ? (
                         <section>
                             {hostVansEls}
